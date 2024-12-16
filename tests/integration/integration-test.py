e0cffd79553b69ca13d06508a8b3619e15674457
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2024-12-16 15:00:30 -0700

    Create integration-test.py

---
+++ tests/integration/integration-test.py
@@ -0,0 +1,6 @@
+def test_integration_placeholder():
+    """
+    A placeholder test to ensure pytest does not fail due to no tests being present.
+    Delete this test and add your own.
+    """
+    assert True
